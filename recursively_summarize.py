9dea78f874a79c2b7ff5da817b192c08b4a4a729
Paul Eden <paul@benchline.org>
2023-01-24 15:08:32 -0700

    Updated to davinci 3 model and removed extra spaces between chunk summaries.

--- recursively_summarize.py
+++ recursively_summarize.py
@@ -18,7 +18,7 @@ def save_file(content, filepath):
         outfile.write(content)
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
     max_retry = 5
     retry = 0
     while True:
@@ -58,4 +58,4 @@ if __name__ == '__main__':
         summary = gpt3_completion(prompt)
         print('\n\n\n', count, 'of', len(chunks), ' - ', summary)
         result.append(summary)
-    save_file('\n\n'.join(result), 'output_%s.txt' % time())
+    save_file('\n'.join(result), 'output_%s.txt' % time())
